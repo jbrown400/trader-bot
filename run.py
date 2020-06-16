f79fd8ae87afa47492a4add8bf9d5eedd35fa33d
jbrown400 <justin.brown400@gmail.com>
2020-06-15 19:56:54 -0500

    I was going to write a whole program but I decided to just start with a Jupyter Notebook. Added some data.

---
+++ run.py
@@ -0,0 +1,19 @@
+from td.client import TDClient
+from config.config import *
+
+
+if __name__ == '__main__':
+
+	TDSession = TDClient(account_number=ACCOUNT_NUMBER,
+	                     client_id=CLIENT_ID,
+	                     redirect_uri=REDIRECT_URI,
+	                     credentials_path=JSON_PATH)
+	TDSession.login()
+
+	price_history = TDSession.get_price_history(symbol='CCL',
+	                                            period_type='year',
+	                                            period=2,
+	                                            frequency='1',
+	                                            frequency_type='daily')
+
+	print(price_history)
