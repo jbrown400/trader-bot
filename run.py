a5d84af2afa1af4c823b2f561bc2e37af63ddd09
jbrown400 <justin.brown400@gmail.com>
2020-09-09 16:45:32 -0500

    Getting started with getting data.

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import time as time_lib
+import pytz
 import pprint
 import pathlib
 import operator
@@ -10,7 +11,6 @@ from datetime import timedelta
 from pyrobot.robot import PyRobot
 from pyrobot.indicators import Indicators
 from configs.config import *
-import tda
 
 
 if __name__ == '__main__':
@@ -21,3 +21,30 @@ if __name__ == '__main__':
 		credentials_path=JSON_PATH,
 		paper_trading=True
 	)
+
+	trading_robot_portfolio = trading_bot.create_portfolio()
+
+	print("Pre market open: ", trading_bot.pre_market_open)
+	print("Regular market open: ", trading_bot.regular_market_open)
+	print("Post market open: ", trading_bot.post_market_open)
+
+	end_date = datetime.today()
+	start_date = end_date - timedelta(days=1)
+
+	trading_robot_portfolio.add_position(
+		symbol='MSFT',
+		asset_type='equity',
+	)
+
+	today_data = trading_bot.grab_historical_prices(
+		start=start_date,
+		end=end_date,
+		bar_size=1,
+		bar_type='minute'
+	)
+
+	stock_frame = trading_bot.create_stock_frame(
+		data=today_data['aggregated']
+	)
+
+	stock_frame.frame.to_csv('./data/MSFT_today.csv', sep=',')
