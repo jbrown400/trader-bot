6eb792200de0babf31ae55936135784299f5a679
jbrown400 <justin.brown400@gmail.com>
2021-04-15 17:48:24 -0500

    Working through how I want to organize things.

--- core/main/Robot.py
+++ core/main/Robot.py
@@ -1,6 +1,33 @@
+from pyrobot.robot import PyRobot
+
+#todo do I want multiple robots and pit them against each other?
+# yes...so do adversarial training and then pick the best one each month..?
 
 class Robot:
 	"""
 	Extends PyRobot class (which contains TD API) to add functionality or quickly implement bug fixes
 	"""
-	pass
+
+	def __init__(self, client_id: str,
+	             redirect_uri: str,
+	             paper_trading: bool = True,
+	             credentials_path: str = None,
+	             trading_account: str = None):
+		self.py_robot = PyRobot(client_id, redirect_uri, paper_trading, credentials_path, trading_account)
+		self.tickers = []
+
+	@property
+	def tickers(self):
+		return self.tickers
+
+	@tickers.setter
+	def tickers(self, value):
+		self._tickers = value
+
+	@property
+	def performance(self):
+		"""
+		Calculate and return current performance metrics...not really sure what that metric will be tho...
+		:return: Performance metric
+		"""
+		return "performance"
